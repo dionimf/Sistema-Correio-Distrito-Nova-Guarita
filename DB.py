eecf992075e7eaeb39288e663311d4b288cc9a06
dioni <dionimf@unesc.net>
2016-03-02 20:05:15 -0300

    inicio do projeto

---
+++ DB.py
@@ -0,0 +1,6 @@
+__author__ = 'Zero'
+class DB(object):
+    def com(self):
+        return True
+    def SQL(self,sql):
+        return True
---
+++ FunctionsS.py
@@ -0,0 +1,17 @@
+__author__ = 'Zero'
+class FunctionS(object):
+    def __init__(self,nome,sobrenome,date,local,endereco,typeE):
+        self.nome=nome
+        self.sobrenome=sobrenome
+        self.date=date
+        self.local=local
+        self.endereco=endereco
+        self.typeE=typeE
+    def Create(self):
+        return
+    def Delete(self):
+        return
+    def Update(self):
+        return
+    def Consult(self):
+        return
